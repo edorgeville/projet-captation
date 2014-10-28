fdd6dce171d6da73b4387d739a091db7289ab9e1
Erwan <th3m4ri0@gmail.com>
2014-10-28 18:35:36 -0400

    Fini connexion arduino

--- arduino_server/app.js
+++ arduino_server/app.js
@@ -29,38 +29,44 @@ var questions2 = [
 var questions3 = [
     {
         name: 'minWind',
+        oriName: 'wind',
         description: 'Minimum wind',
         default: 0,
         type: 'number',
     },
     {
         name: 'maxWind',
+        oriName: 'wind',
         description: 'Maximum wind',
-        default: 0,
+        default: 500,
         type: 'number',
     },
     {
         name: 'minFire',
+        oriName: 'fire',
         description: 'Minimum fire',
         default: 0,
         type: 'number',
     },
     {
         name: 'maxFire',
+        oriName: 'fire',
         description: 'Maximum fire',
-        default: 0,
+        default: 100,
         type: 'number',
     },
     {
         name: 'minWater',
+        oriName: 'water',
         description: 'Minimum water',
         default: 0,
         type: 'number',
     },
     {
         name: 'maxWater',
+        oriName: 'water',
         description: 'Maximum water',
-        default: 0,
+        default: 500,
         type: 'number',
     }
 ];
@@ -98,31 +104,90 @@ continuerQuestions2();
 
 function continuerQuestions2(){
     serialport.list(function (err, ports) {
-        var first = true;
+        // var first = true;
         console.log('Available serial ports :')
         ports.forEach(function(port) {
             console.log(" " + port.comName);
-            if(first){
+            // if(first){
                 questions2[0].default = port.comName;
-                first = false;
-            }
+                // first = false;
+            // }
         });
         console.log('');
         prompt.get(questions2, function (err, result) {
             if (err) { return onErr(err); }
             saveOption(result);
+            startSerial();
             continuerQuestions3();
         });
     });
+}
+
+var serial;
+
+var lastReceived = {};
+
+function startSerial() {
+    // Connect to Arduino
+    // Look for newline delimiter
+    console.log("ARDUINO > Connecting to port: "+ options.port);
+    serial = new serialport.SerialPort( options.port, {parser: serialport.parsers.readline( '\n' )  } );
+    
+    serial.on('data', function(data) {
+        try{
+            var splitted = data.split(' ');
+            var name = splitted[0].replace("\r", "");
+            var content = Number(splitted[1].replace("\r", ""));
+            lastReceived[name] = content;
+            // console.log(lastReceived);
+            if(started)
+                sendData(name);
+        }
+        catch(err){}
+    });
+}
+
+function sendData(name){
+    socket.emit('event', {
+        type: name,
+        value: filter(name, lastReceived[name]),
+    });
+    if(name == "wind")
+        console.log(name + ": " + filter(name, lastReceived[name]));
+}
+
+function filter(name, data){
+    var min = options["min" + capitaliseFirstLetter(name)] || 0;
+    var max = options["max" + capitaliseFirstLetter(name)] || 0;
+
+    if(data < min){
+        data = min;
+    }else if(data > max){
+        data = max;
+    }
+
+    var newMin = 0;
+    var newMax = 100;
+
+    var mapped = (data - min) / (max - min) * (newMax - newMin) + newMin;
+    
+    
+    // var mapped = ((newMax - newMin) * (data - min) / (max - min)) + newMin;
+
+
+    return mapped;
+}
+
+function capitaliseFirstLetter(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 function continuerQuestions3(){
     function measure(question, next){
         var count = 0;
         var interval = setInterval(function(){
-            //TODO LOG MEASURE
-            console.log(count);
-            if(count > 20){
+            console.log(lastReceived[question.oriName]);
+            if(count > 0){
                 clearInterval(interval);
                 // next();
                 prompt.get(question, function (err, result) {
@@ -150,7 +215,10 @@ function continuerQuestions3(){
     });
 }
 
+var started = false;
+
 function start(){
+    started = true;
     console.log('');
     console.log('--------> Étalonnage terminé');
     console.log('');
@@ -161,12 +229,12 @@ function start(){
       console.log('Connected!');
     });
 
-    setInterval(function(){
-        var value = Math.floor(Math.random() * 100);
-        console.log(value);
-        var e = {};
-        e.type = "wind";
-        e.value = value;
-        socket.emit('event', e);
-    }, 1000);
+    // setInterval(function(){
+    //     var value = Math.floor(Math.random() * 100);
+    //     console.log(value);
+    //     var e = {};
+    //     e.type = "wind";
+    //     e.value = value;
+    //     socket.emit('event', e);
+    // }, 1000);
 }
