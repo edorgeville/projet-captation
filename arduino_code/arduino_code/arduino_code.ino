7505eda16c7029fb4133075935c2ddcf8383abe7
rubanmauve <rubymaude.rioux@gmail.com>
2014-10-28 19:11:39 -0400

    Code arduino avec les bons senseurs

---
+++ arduino_code/arduino_code/arduino_code.ino
@@ -0,0 +1,100 @@
+#define input_water 1
+#define input_wind 2
+#define input_fire A0
+#define resistor 10000
+
+
+
+
+
+// track the state of the pulse pin
+volatile uint8_t lastflowpinstate;
+// you can try to keep time of how long it is between pulses
+volatile uint32_t lastflowratetimer = 0;
+// and use that to calculate a flow rate
+volatile float flowrate;
+
+
+
+
+
+  SIGNAL(TIMER0_COMPA_vect) {
+    uint8_t x = digitalRead(input_water);
+
+    if (x == lastflowpinstate) {
+      lastflowratetimer++;
+      return; // nothing changed!
+    }
+
+    lastflowpinstate = x;
+    flowrate = 1000.0;
+    flowrate /= lastflowratetimer;  // in hertz
+    lastflowratetimer = 0;
+  }
+
+
+
+
+void setup(){
+  Serial.begin(9600);
+
+  pinMode(input_water, INPUT);
+  digitalWrite(input_water, HIGH);
+  lastflowpinstate = digitalRead(input_water);
+
+  useInterrupt(true);
+}
+
+
+
+void useInterrupt(boolean v) {
+  if (v) {
+    // Timer0 is already used for millis() - we'll just interrupt somewhere
+    // in the middle and call the "Compare A" function above
+    OCR0A = 0xAF;
+    TIMSK0 |= _BV(OCIE0A);
+  } 
+  else {
+    // do not call the interrupt function COMPA anymore
+    TIMSK0 &= ~_BV(OCIE0A);
+  }
+}
+
+
+
+
+
+
+
+void loop(){
+
+  /////////////////////////////////////////////////////////////////////WATER
+  Serial.print("water ");
+  Serial.println(flowrate);
+
+
+  /////////////////////////////////////////////////////////////////////FIRE
+  float read_fire;
+
+  read_fire = analogRead(input_fire);
+
+  read_fire = (1023/ read_fire) -1;
+  read_fire = resistor / read_fire;
+
+  Serial.print("fire ");
+  Serial.println(read_fire);
+
+delay(1000);
+  /////////////////////////////////////////////////////////////////////WIND
+  //To be continued
+
+
+}
+
+
+
+
+
+
+
+
